5addd0c5a45afa865dfca621fe0fbbc291db89ca
emmaSUNnydays <1sunemm@hdsb.ca>
2025-02-24 14:27:12 -0500

    Defined variables and wrote print statements
    Defined variables and wrote print statements that declare what different data types are and an example of each.

---
+++ mathOperators.py
@@ -0,0 +1,27 @@
+import math
+
+# This is a square root function.
+square = math.sqrt(25)
+
+# This is addition.
+sumAdd = 2+23
+
+# This is subtraction.
+difference = 2-23
+
+# This is multiplication.
+product = 2*3*4
+
+# This is division.
+quotient = 25/8
+
+# This is 2 to the power of 4.
+exponent = 2**4
+
+# This is 3 examples of modulus/remainder.
+rem1 = 24%6
+rem2 = 24%5
+rem3 = 24%7
+
+x = 9 - 3 / (9/3) + 1
+print(x)
---
+++ typeWork.py
@@ -0,0 +1,24 @@
+# Author: Emma Sun
+# Date Modified: 2025/02/24
+# Description: Describe different data types.
+
+# these are examples of data types
+integer = 15
+float = 4.5
+char = 'e'
+boolean = True
+string = "joe"
+
+# these are definitions of data types
+integerDefinition = "any positive or negative whole number"
+floatDefinition = "number with a decimal"
+charDefinition = "single character"
+booleanDefinition = "true or false"
+stringDefinition = "a combination of characters"
+
+# these are print statements that define and provide an example for each data type.
+print(f"An integer is {integerDefinition} and an example is {integer}.")
+print(f"A float is a {floatDefinition} and an example is {float}.")
+print(f"A character/char is a {charDefinition} and an example is {char}.")
+print(f"A boolean is {booleanDefinition} and an example is {boolean}.")
+print(f"A string is {stringDefinition} and an example is {string}.")
